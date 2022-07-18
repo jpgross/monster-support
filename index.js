c57a777cbb9c549c9a2ca146ac9683cb5bd9e12b
Jason Gross <jpgross@gmail.com>
2022-07-17 23:52:28 -0400

    hmmmm

--- index.js
+++ index.js
@@ -4,24 +4,5 @@ const gameSchema = new mongoose.Schema({
     name1: String, name2: String, name3: String, name4: String 
 });
 gameSchema.set('toJSON', { getters: true, virtuals: false });
-const Game = mongoose.model('Game', gameSchema);
 
-//TODO some maddening stuff here with options/defaults...
-async function createGame(playerName, env) {
-
-    try {
-        mongoose.connect(env['DATABASE_URL'], env['DATABASE_NAME']);
-        const game = new Game({ name1: playerName});
-        await game.save();
-
-        console.log(`created game ${game._id}`);
-
-        return game;
-    } catch (e) {
-        console.error('error during save', e);
-        throw e;
-    }
-
-}
-
-module.exports.createGame = createGame;
+module.exports.schema.Game = mongoose.model('Game', gameSchema);
